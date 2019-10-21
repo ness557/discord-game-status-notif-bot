06a966d8c95ad2fc3b2b73d01a07a39c575f107b
ness557 <ro.bezruchko@andersenlab.com>
2019-10-21 17:15:07 +0300

    tkn

--- bot.js
+++ bot.js
@@ -1,27 +1,28 @@
 ﻿var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+	token: process.env.BOT_TOKEN,
+	autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ' + bot.username);
+	logger.info('Connected');
+	logger.info('Logged in as: ' + bot.username);
 });
 // onGameChange hook
 bot.on('presence', function (user, userID, status, game, event) {
 	logger.info(user + ' changed somehow!');
 	// bot.sendMessage({ to: '456917371882307605', message: user + ' changed somehow!'});
 	if (game != null) {
-		bot.sendMessage({ to: '456917371882307605',
-					message: user + ' отправился в ' + game.name + '!' });
+		bot.sendMessage({
+			to: '456917371882307605',
+			message: user + ' отправился в ' + game.name + '!'
+		});
 	}
 });
